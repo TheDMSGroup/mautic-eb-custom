70bbaf65f97470221605d99137e0ca340e151530
heathdutton <hdutton@thedmsgrp.com>
2018-01-31 14:16:08 -0500

    Added jira issue collector

---
+++ app/bundles/CoreBundle/Assets/js/99.jira.js
@@ -0,0 +1,6 @@
+mQuery.ajax({
+    url: "https://thedmsgrp.atlassian.net/s/d41d8cd98f00b204e9800998ecf8427e-T/-aef0j9/b/8/e73395c53c3b10fde2303f4bf74ffbf6/_/download/batch/com.atlassian.jira.collector.plugin.jira-issue-collector-plugin:issuecollector-embededjs/com.atlassian.jira.collector.plugin.jira-issue-collector-plugin:issuecollector-embededjs.js?locale=en-US&collectorId=e569fc9a",
+    type: "get",
+    cache: true,
+    dataType: "script"
+});
