c5d2b04ef97a5d215fbf61315e81e3d3b8e3cf40
heathdutton <hdutton@thedmsgrp.com>
2018-03-06 15:19:08 -0500

    Prevent Mautic ajax collision: flashes

--- app/bundles/CoreBundle/Assets/js/98.statuspage.js
+++ app/bundles/CoreBundle/Assets/js/98.statuspage.js
@@ -8,7 +8,7 @@
 
                 var script = document.createElement('script');
                 var prior = document.getElementsByTagName('script')[0];
-                script.async = 1;
+                script.async = true;
                 script.onload = script.onreadystatechange = function (_, isAbort) {
                     if (isAbort || !script.readyState || /loaded|complete/.test(script.readyState)) {
                         script.onload = script.onreadystatechange = null;
--- app/bundles/CoreBundle/Assets/js/99.jira.js
+++ app/bundles/CoreBundle/Assets/js/99.jira.js
@@ -1,6 +1,10 @@
-mQuery.ajax({
-    url: "https://thedmsgrp.atlassian.net/s/d41d8cd98f00b204e9800998ecf8427e-T/-aef0j9/b/8/e73395c53c3b10fde2303f4bf74ffbf6/_/download/batch/com.atlassian.jira.collector.plugin.jira-issue-collector-plugin:issuecollector-embededjs/com.atlassian.jira.collector.plugin.jira-issue-collector-plugin:issuecollector-embededjs.js?locale=en-US&collectorId=e569fc9a",
-    type: "get",
-    cache: true,
-    dataType: "script"
-});
+// Add standard Jira support widget.
+(function ($) {
+    $(document).ready(function () {
+        var script = document.createElement('script');
+        var prior = document.getElementsByTagName('script')[0];
+        script.async = true;
+        script.src = 'https://thedmsgrp.atlassian.net/s/d41d8cd98f00b204e9800998ecf8427e-T/-aef0j9/b/8/e73395c53c3b10fde2303f4bf74ffbf6/_/download/batch/com.atlassian.jira.collector.plugin.jira-issue-collector-plugin:issuecollector-embededjs/com.atlassian.jira.collector.plugin.jira-issue-collector-plugin:issuecollector-embededjs.js?locale=en-US&collectorId=e569fc9a';
+        prior.parentNode.insertBefore(script, prior);
+    });
+})(mQuery);
