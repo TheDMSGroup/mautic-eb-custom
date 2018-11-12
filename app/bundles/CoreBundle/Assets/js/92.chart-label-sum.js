5eb2d618d7f9d11ce29ef8f7f84def7171a8bf7c
scottshipman <k.scott.shipman@gmail.com>
2018-11-12 10:35:09 -0500

    add dataTables injection into campaign, contact, and source pages stubbed out

--- app/bundles/CoreBundle/Assets/js/92.chart-label-sum.js
+++ app/bundles/CoreBundle/Assets/js/92.chart-label-sum.js
@@ -46,12 +46,69 @@ Mautic.chartLabelSum = function () {
     }
 };
 
+// Append dataTables to Charts on Campaign, Source and Client pages.
+Mautic.appendTableToCharts = function () {
+    var mauticContent;
+
+    if ((["contactsource", "campaign", "contactclient"]).indexOf(mauticContent) > -1) {
+
+        if (
+            typeof Mautic.chartObjects !== 'undefined'
+            && Mautic.chartObjects.length
+        ) {
+            mQuery.each(Mautic.chartObjects, function (index, chart) {
+
+                if (
+                    typeof chart.data.datasets !== 'undefined'
+                    && (
+                        typeof chart.tableAdded === 'undefined'
+                        || chart.tableAdded === false
+                    )
+                ) {
+
+                    // add the table element for datatables to use
+                    mQuery(chart.canvas.parentElement).append('<table id="chart-' + index + '"></table>');
+
+                    //convert chart dataset to dataTables json
+                    var headers = ['Date'];
+                    var rows = [];
+                    var rowsCount = chart.data.labels.length;
+
+                    for (c = 0; c < rowsCount; c++) {
+                        mQuery.each(chart.data.datasets, function (i, dataset) {
+                            headers.push(dataset.label);
+                            row = [chart.data.labels[c]];
+                            row.push(dataset.data[c]);
+                        });
+                        rows.push(row);
+                    }
+
+                    // invoke dataTables
+                    mQuery("#chart-" + index).DataTable({
+                        data: rows,
+                        autoFill: true,
+                        columns: headers,
+
+                    });
+
+                    // mark chart as table-added
+                    chart.tableAdded = true;
+                }
+            });
+        }
+    }
+};
+
+
+
 mQuery(document).ready(function () {
     Mautic.chartLabelSum();
+    Mautic.appendTableToCharts();
 });
 
 mQuery(document).ajaxComplete(function (event, xhr, settings) {
     setTimeout(function () {
         Mautic.chartLabelSum();
+        Mautic.appendTableToCharts();
     }, 150);
 });
